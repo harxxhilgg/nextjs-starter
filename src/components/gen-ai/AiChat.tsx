6840ac499d1a45c124da25e1e5ec74df296296f4
Harshil Patel <harshilp046@gmail.com>
2026-02-24 13:47:16 +0530

    feat: enhance AIChat form validation and error handling with improved user feedback

--- src/components/gen-ai/AiChat.tsx
+++ src/components/gen-ai/AiChat.tsx
@@ -7,14 +7,16 @@ import {
   Field,
   FieldContent,
   FieldError,
+  FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { roastSchema, RoastValues } from "@/lib/schemas";
 import { Spinner } from "../ui/spinner";
 import { Slider } from "../ui/slider";
+import { toast } from "sonner";
+import { InputGroup, InputGroupAddon, InputGroupText, InputGroupTextarea } from "../ui/input-group";
 
 export default function AIChat() {
   const [roast, setRoast] = useState<string[]>([]);
@@ -22,6 +24,7 @@ export default function AIChat() {
 
   const form = useForm<RoastValues>({
     resolver: zodResolver(roastSchema),
+    mode: "onChange",
     defaultValues: {
       name: "",
       bio: "",
@@ -33,75 +36,90 @@ export default function AIChat() {
     setLoading(true);
     setRoast([]);
 
-    const res = await fetch("/api/ai", {
-      method: "POST",
-      body: JSON.stringify(values),
-    });
+    try {
+      const res = await fetch("/api/ai", {
+        method: "POST",
+        body: JSON.stringify(values),
+      });
 
-    const data = await res.json();
-    setRoast(data.roast ?? []);
-    setLoading(false);
+      const data = await res.json();
+      setRoast(data.roast ?? []);
+
+      if (res.ok) {
+        form.reset();
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+
+      toast.error("Error", {
+        description: "Something went wrong, please try again after sometime.",
+      });
+    } finally {
+      setLoading(false);
+    };
   }
 
   return (
-    <div>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-10 items-center mb-10"
-      >
-        {/* NAME */}
-        <Field>
-          <FieldLabel htmlFor="name">Name</FieldLabel>
-          <FieldContent>
-            <Input
-              id="name"
-              placeholder="Enter your name... (i.e.: Jeffery Epstein)"
-              {...form.register("name")}
-            />
-          </FieldContent>
-          <FieldError errors={form.formState.errors.name ? [form.formState.errors.name] : undefined} />
-        </Field>
-
-        {/* BIO */}
-        <Field>
-          <FieldLabel htmlFor="bio">Bio</FieldLabel>
-          <FieldContent>
-            <Textarea
-              id="bio"
-              placeholder="Enter your proffesion or your idea... (i.e.: Cameraman)"
-              {...form.register("bio")}
-            />
-          </FieldContent>
-          <FieldError errors={form.formState.errors.bio ? [form.formState.errors.bio] : undefined} />
-        </Field>
-
-        {/* LEVEL */}
-        {/* <Field>
-          <FieldLabel htmlFor="level">Intensity</FieldLabel>
-          <FieldContent>
+    <div className="space-y-6">
+      <form id="form-rhf-roast" onSubmit={form.handleSubmit(onSubmit)}>
+        <FieldGroup>
+          <div className="flex flex-col gap-8 items-center mb-8">
             <Controller
-              name="level"
+              name="name"
               control={form.control}
-              render={({ field }) => (
-                <Select
-                  value={field.value}
-                  onValueChange={field.onChange}
-                >
-                  <SelectTrigger id="level">
-                    <SelectValue />
-                  </SelectTrigger>
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-name" className="gap-0.5">
+                    Name <span>*</span>
+                  </FieldLabel>
 
-                  <SelectContent>
-                    <SelectItem value="mild">Mild 🙂</SelectItem>
-                    <SelectItem value="medium">Medium 😈</SelectItem>
-                    <SelectItem value="savage">Savage 🔥</SelectItem>
-                  </SelectContent>
-                </Select>
+                  <Input
+                    {...field}
+                    id="form-rhf-name"
+                    placeholder="Enter your name... (i.e.: Jeffery Epstein)"
+                  />
+
+                  {fieldState.invalid && (
+                    <FieldError errors={form.formState.errors.name ? [form.formState.errors.name] : undefined} />
+                  )}
+                </Field>
               )}
             />
-          </FieldContent>
-          <FieldError errors={form.formState.errors.level ? [form.formState.errors.level] : undefined} />
-        </Field> */}
+
+            <Controller
+              name="bio"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-bio" className="gap-0.5">
+                    Bio <span>*</span>
+                  </FieldLabel>
+
+                  <InputGroup>
+                    <InputGroupTextarea
+                      {...field}
+                      id="form-rhf-bio"
+                      placeholder="Tell me about you or your profession..."
+                      rows={4}
+                      className="min-h-24 resize-none"
+                      aria-invalid={fieldState.invalid}
+                    />
+
+                    <InputGroupAddon align="block-end">
+                      <InputGroupText>
+                        {field.value.length}/200 characters
+                      </InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </div>
+        </FieldGroup>
 
         <Field>
           <FieldLabel htmlFor="level">Roast Intensity</FieldLabel>
@@ -148,9 +166,10 @@ export default function AIChat() {
 
         <Button
           type="submit"
+          form="form-rhf-roast"
           disabled={loading}
           variant="default"
-          className="cursor-pointer"
+          className="flex mt-10 mx-auto cursor-pointer w-40"
         >
           {loading && (
             <Spinner />
@@ -162,21 +181,19 @@ export default function AIChat() {
 
       {/* RESULT */}
       {roast.length > 0 && (
-        <h2 className="text-xl font-semibold">Result:</h2>
+        <h2 className="text-xl font-semibold">Results:</h2>
       )}
 
       {roast.length > 0 && (
-        <ul className="mt-8 space-y-3 mb-10">
+        <ul className="space-y-3 mb-10">
           {roast.map((item, index) => (
-            <>
-              <li key={index} className="flex gap-6">
-                <span className="mt-2 h-1.5 w-1.5 rounded-full bg-secondary/70 shrink-0" />
+            <li key={index} className="flex gap-6">
+              <span className="mt-2 h-1.5 w-1.5 rounded-full bg-secondary/70 shrink-0" />
 
-                <p className="tracking-[0.005em]">
-                  {item}
-                </p>
-              </li>
-            </>
+              <p className="tracking-[0.005em]">
+                {item}
+              </p>
+            </li>
           ))}
         </ul>
       )}
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,8 +1,14 @@
 import * as z from "zod";
 
 export const roastSchema = z.object({
-  name: z.string().min(2, "Name required"),
-  bio: z.string().optional(),
+  name: z
+    .string()
+    .min(3, "Name must be at least 3 characters")
+    .max(50, "Name must be at most 50 characters"),
+  bio: z
+    .string()
+    .min(2, "Bio must be at least 2 characters")
+    .max(200, "Bio must be at most 200 characters"),
   level: z.enum(["mild", "medium", "savage"]),
 });
 
